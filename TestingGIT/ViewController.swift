b8c346a1c6861414f21486b4e4e12c3312e70429
Kobi Naparstek <kobin@plymedia-macbook-pro.local>
2016-11-15 14:20:19 +0200

    Initial Commit

---
+++ TestingGIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingGIT
+//
+//  Created by Kobi Naparstek on 15/11/2016.
+//  Copyright © 2016 Kobi Naparstek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
