ae9ab6b1ca8d60ae0db68ff3896c31c62c444c57
Kobi Naparstek <kobin@plymedia-macbook-pro.local>
2016-11-15 15:42:04 +0200

    Added SomeNewObject in feature branch

---
+++ TestingGIT/SomeNewObject.swift
@@ -0,0 +1,13 @@
+//
+//  SomeNewObject.swift
+//  TestingGIT
+//
+//  Created by Kobi Naparstek on 15/11/2016.
+//  Copyright © 2016 Kobi Naparstek. All rights reserved.
+//
+
+import UIKit
+
+class SomeNewObject: NSObject {
+
+}
