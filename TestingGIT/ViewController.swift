a551d40c45f3d3894776aca4ed08d01ee73efe74
Kobi Naparstek <kobin@PLYMedia-MacBook-Pro.local>
2016-11-16 11:11:48 +0200

    is there something to commit?

--- TestingGIT/ViewController.swift
+++ TestingGIT/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func tmpFunc() {
+        
+        print("Some chanmge")
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
