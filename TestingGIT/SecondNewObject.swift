dad385cfa4b916bfd9b12229c2f4b3e6f51082cd
Kobi Naparstek <kobin@PLYMedia-MacBook-Pro.local>
2016-11-16 11:08:18 +0200

    commit command  after a source change plus an added file

---
+++ TestingGIT/SecondNewObject.swift
@@ -0,0 +1,13 @@
+//
+//  SecondNewObject.swift
+//  TestingGIT
+//
+//  Created by Kobi Naparstek on 16/11/2016.
+//  Copyright © 2016 Kobi Naparstek. All rights reserved.
+//
+
+import UIKit
+
+class SecondNewObject: NSObject {
+
+}
