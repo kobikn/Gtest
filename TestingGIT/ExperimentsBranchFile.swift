5a03433b4cff81da295b58f41f9a0be61dbeea36
Kobi Naparstek <kobin@PLYMedia-MacBook-Pro.local>
2016-11-21 11:41:02 +0200

    First commiting to experiment brach

---
+++ TestingGIT/ExperimentsBranchFile.swift
@@ -0,0 +1,13 @@
+//
+//  ExperimentsBranchFile.swift
+//  TestingGIT
+//
+//  Created by Kobi Naparstek on 21/11/2016.
+//  Copyright © 2016 Kobi Naparstek. All rights reserved.
+//
+
+import UIKit
+
+class ExperimentsBranchFile: NSObject {
+
+}
